2131f14ea465678930a69ff61b0d6d1158ceb774
AlexStorm <info@alexstorm.ru>
2023-03-04 17:28:44 +0530

    build fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from "@angular/router";
-import { BootstrapComponent, FuiTemplatesModule, TABLE_PAGE } from "@solenopsys/ui-templates";
+import { BootstrapComponent, UITemplatesModule, TABLE_PAGE } from "@solenopsys/ui-templates";
 import { ObjectsStatComponent } from "./objects-stat/objects-stat.component";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TABLES_CONFS } from "./tables.config";
-import { FuiGridModule } from "@solenopsys/ui-lists";
+import { UIListsModule } from "@solenopsys/ui-lists";
 import { createNgxs } from "@solenopsys/fl-storage";
 import { environment } from "../environments/environment";
 import { FormsModule } from "@angular/forms";
@@ -28,8 +28,8 @@ export const PROVIDERS_CONF = [
 export const IMPORTS_CONF = [
   CommonModule,
   BrowserModule,  FormsModule,
-  FuiGridModule,
-  FuiTemplatesModule,
+  UIListsModule,
+  UITemplatesModule,
   RouterModule.forChild(routes),
   ...createNgxs(!environment.production),
 ]
